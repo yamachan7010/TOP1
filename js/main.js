921c50637fcd19c42a68ee1e9a06a6146937bc5b
yamachan7010 <exabsd5867@yahoo.co.jp>
2022-03-23 15:01:05 +0900

    3月23日変更

--- js/main.js
+++ js/main.js
@@ -6,4 +6,13 @@ $(function() {
         //$(this)はイベントが発生した要素
         $("body").toggleClass("is-active");
     });
+
+    $(document).ready(function(){
+        $('.js-slick').slick({
+            
+            autoplay: true,
+            dots: true,
+            autoplaySpeed: 3000,
+        });
+    });
 });
