f8147b940a90f91bab3ee2c4a640512387ef9366
yamachan7010 <exabsd5867@yahoo.co.jp>
2022-04-02 14:40:48 +0900

    4月2日変更

--- footer.php
+++ footer.php
@@ -1,9 +1,4 @@
 <!-- jQueryの読み込み（CDN） -->
-<script src="//ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/slick/slick.js"></script>
-<!-- main.jsの読み込み -->
-<script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
-
-
 
 <?php wp_footer(); ?>
---
+++ functions.php
@@ -0,0 +1,29 @@
+<?php
+//タイトルタグの出力
+add_theme_support('title-tag');
+
+//アイキャッチ画像設定の有効化
+add_theme_support('post-thumbnails');
+
+function load_script() {
+    if ( !is_admin() ) {
+        // !is_admin() 管理画面ではない時の画面
+        // ここでファイルの読み込みを行う
+
+        //reset.cssの読み込み
+        wp_enqueue_style(
+            'reset-css',
+            get_template_directory_uri() . '/css/reset.css',
+        );
+
+        //main.jsの読み込み
+        wp_enqueue_script(
+            'main-script',
+            get_template_directory_uri() . '/js/main.js',
+            array('jquery'),
+            false,
+            true
+        );
+    }
+}
+add_action('init', 'load_script');
